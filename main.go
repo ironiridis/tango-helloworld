550a37b36d9e451c047cfc0178b5f69a810d67d8
Chris Harrington <ironiridis@gmail.com>
2019-11-30 20:44:11 -0600

    start actually trying to use the RPC interface

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os"
+
+	"github.com/ironiridis/tango/rpc"
+)
+
+func main() {
+	var err error
+	h := rpc.NewHandle(os.Stdout, os.Stdin)
+
+	hellomsg := rpc.UIDefineCategory{Label: "Hello"}
+	err = h.Send(&hellomsg)
+	if err != nil {
+		panic(err)
+	}
+
+}
